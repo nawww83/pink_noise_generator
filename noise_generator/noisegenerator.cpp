0c99a470c9da50c2eaca5f317703d3943dd4fe35
Anatoly V Novikov <nawww83@gmail.com>
2025-04-02 21:10:54 +0300

    Add DC offset correction

--- noise_generator/noisegenerator.cpp
+++ noise_generator/noisegenerator.cpp
@@ -1,5 +1,6 @@
 #include "noisegenerator.h"
 #include <cmath>
+// #include <qdebug.h>
 
 template <typename T>
 NoiseGenerator<T>::NoiseGenerator(int fir_order): mFirOrder(fir_order) {
@@ -9,19 +10,19 @@ NoiseGenerator<T>::NoiseGenerator(int fir_order): mFirOrder(fir_order) {
 template<typename T>
 T NoiseGenerator<T>::NextSample(T input)
 {
-    T value = 0;
-    int offset = 0;
+    T iir_output = 0;
+    int idx_offset = 0;
     for (int k = 0; k < NUM_OF_IIRS; k++) {
-        mIirOutput[0 + offset] = input * std::exp(static_cast<T>(-1)) + mIirOutput[0 + offset] * std::exp(static_cast<T>(-1));
-        T inner_out = mIirOutput[0 + offset];
+        mIirState[0 + idx_offset] = input * std::exp(static_cast<T>(-1)) + mIirState[0 + idx_offset] * std::exp(static_cast<T>(-1));
+        T inner_out = mIirState[0 + idx_offset];
         const auto half = mIirSettings.mR[k]/2;
         T attenuation1 = std::exp(-mIirSettings.mTau[k]);
         const T cfactor1 = std::pow(mIirSettings.mTau[k], mIirSettings.mPowers[k]);
         T factor1 = cfactor1;
         for (int j = 1; j <= half; ++j) {
             const T gain = factor1 * attenuation1;
-            mIirOutput[j + offset] = gain * input + mIirOutput[j + offset] * attenuation1;
-            inner_out += mIirOutput[j + offset];
+            mIirState[j + idx_offset] = gain * input + mIirState[j + idx_offset] * attenuation1;
+            inner_out += mIirState[j + idx_offset];
             factor1 *= cfactor1;
             attenuation1 = std::pow(attenuation1, mIirSettings.mTau[k]);
         }
@@ -30,21 +31,20 @@ T NoiseGenerator<T>::NextSample(T input)
         T factor2 = cfactor2;
         for (int j = 1; j <= half; ++j) {
             const T gain = factor2 * attenuation2;
-            mIirOutput[j + offset + half] = gain * input + mIirOutput[j + offset + half] * attenuation2;
-            inner_out += mIirOutput[j + offset + half];
+            mIirState[j + idx_offset + half] = gain * input + mIirState[j + idx_offset + half] * attenuation2;
+            inner_out += mIirState[j + idx_offset + half];
             factor2 *= cfactor2;
             attenuation2 = std::pow(attenuation2, T(1)/mIirSettings.mTau[k]);
         }
         inner_out /= mScales[k];
-        value += mIirSettings.mCoeffs[k] * inner_out;
-        offset += mIirSettings.mR[k];
+        iir_output += mIirSettings.mCoeffs[k] * inner_out;
+        idx_offset += mIirSettings.mR[k];
     }
     const auto norma = std::sqrt(std::numbers::pi);
-    value /= norma;
+    iir_output /= norma;
     // DC offset коррекция.
-    // constexpr T epsilon = 1.71e-9; // new DC offset -120 dB vs -64 dB: no change sign.
-    constexpr T epsilon = 1.90e-9; // changed sign: overload.
-    value += (mCorrector[0] - mCorrector[1])*epsilon;
+    constexpr T epsilon = 1.71e-9; // Остаточный DC offset: -120 дБ vs -64 дБ.
+    iir_output += (mCorrector[0] - mCorrector[1])*epsilon;
     mCorrector[0] -= mCorrector[0]*T(1.e-6);
     mCorrector[1] -= mCorrector[1]*T(2.e-6);
     //
@@ -53,11 +53,20 @@ T NoiseGenerator<T>::NextSample(T input)
     }
     mFirState[0] = input;
     for (int i = 0; i < mFirOrder; ++i) {
-        value += mFirCoeffs[i] * mFirState[i];
+        iir_output += mFirCoeffs[i] * mFirState[i];
     }
     T output = mPrevSample + T(1) * input - T(1)/T(2) * mPrevFilters;
     mPrevSample = output;
-    mPrevFilters = value;
+    mPrevFilters = iir_output;
+    mDCoffset += output;
+    mSampleCounter++;
+    if (mSampleCounter % DC_OFFSET_N == 0) {
+        mDCoffset /= T(DC_OFFSET_N);
+        // qDebug() << "DC offset: " << mDCoffset;
+        mPrevSample -= mDCoffset; // Коррекция накопленного паразитного смещения.
+        mDCoffset = 0;
+        mSampleCounter = 0;
+    }
     return output;
 }
 
@@ -110,28 +119,30 @@ void NoiseGenerator<T>::SetIirSettings(IirSettings<T> settings)
 template<typename T>
 void NoiseGenerator<T>::Update()
 {
+    mSampleCounter = 0;
     mCorrector[0] = 1.;
     mCorrector[1] = 1.;
     mPrevSample = 0;
     mPrevFilters = 0;
+    mDCoffset = 0;
     const auto& c_3_2_exact = CalculateSequence_3_2(mFirOrder);
     int iir_order = std::accumulate(mIirSettings.mR, mIirSettings.mR + NUM_OF_IIRS, 0);
-    mIirOutput.resize(iir_order); mIirOutput.fill(1);
+    mIirState.resize(iir_order); mIirState.fill(1);
     mIir_3_2.clear();
     for (int i = 0; i < mFirOrder; ++i) {
         T out = 0;
         int offset = 0;
         for (int k = 0; k < NUM_OF_IIRS; k++) {
-            mIirOutput[0 + offset] *= std::exp(static_cast<T>(-1));
-            T inner_out = mIirOutput[0 + offset];
+            mIirState[0 + offset] *= std::exp(static_cast<T>(-1));
+            T inner_out = mIirState[0 + offset];
             const auto half = mIirSettings.mR[k]/2;
             T attenuation1 = std::exp(-mIirSettings.mTau[k]);
             const T cfactor1 = std::pow(mIirSettings.mTau[k], mIirSettings.mPowers[k]);
             T factor1 = cfactor1;
             for (int j = 1; j <= half; ++j) {
-                mIirOutput[j + offset] *= attenuation1;
+                mIirState[j + offset] *= attenuation1;
                 const T gain = factor1;
-                inner_out += gain * mIirOutput[j + offset];
+                inner_out += gain * mIirState[j + offset];
                 factor1 *= cfactor1;
                 attenuation1 = std::pow(attenuation1, mIirSettings.mTau[k]);
             }
@@ -139,9 +150,9 @@ void NoiseGenerator<T>::Update()
             const T cfactor2 = std::pow(mIirSettings.mTau[k], -mIirSettings.mPowers[k]);
             T factor2 = cfactor2;
             for (int j = 1; j <= half; ++j) {
-                mIirOutput[j + offset + half] *= attenuation2;
+                mIirState[j + offset + half] *= attenuation2;
                 const T gain = factor2;
-                inner_out += gain * mIirOutput[j + offset + half];
+                inner_out += gain * mIirState[j + offset + half];
                 factor2 *= cfactor2;
                 attenuation2 = std::pow(attenuation2, T(1)/mIirSettings.mTau[k]);
             }
@@ -164,7 +175,7 @@ void NoiseGenerator<T>::Update()
     for (int i = 0; i < mIir_3_2.size(); ++i) {
         mFirCoeffs.push_back(c_3_2_exact.at(i) - mIir_3_2.at(i));
     }
-    mIirOutput.resize(iir_order); mIirOutput.fill(0);
+    mIirState.resize(iir_order); mIirState.fill(0);
     mIir_3_2.clear();
     mFirState.resize(mFirOrder); mFirState.fill(0);
 }
